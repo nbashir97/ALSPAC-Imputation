58c60756ccfc6f85accb9a7f34d11df543c7d492
Nasir <84186827+nbashir97@users.noreply.github.com>
2022-08-01 02:02:58 +0100

    Update preimputation.sh

--- round1/g0m/preimputation.sh
+++ round1/g0m/preimputation.sh
@@ -36,6 +36,9 @@ plink --bfile ${data}/g0m/data/originals/g0m --missing
 ### 17,842 individuals / 465,740 variants
 ### 0.999215 genotyping rate / 843 het. haploid genotypes
 
+# Check for any issues with mismatched sex
+plink --bfile ${data}/g0m/data/originals/g0m --check-sex
+
 ## Creating map file from bim file
 cut -f 1-4 ${data}/g0m/data/originals/g0m.bim > \
 ${data}/g0m/data/preimputation/g0m.bim.map
